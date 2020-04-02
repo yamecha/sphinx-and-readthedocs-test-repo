f39f14a628cd14cf75a406bbf290d99475ef30e2
shinseitaro <shinsei.all@gmail.com>
2020-04-02 17:48:04 +0900

    read the doc 用に master_doc='index' 設定を追加

--- source/conf.py
+++ source/conf.py
@@ -57,3 +57,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# https://stackoverflow.com/a/56448499
+# Sphinxは master doc を [contents] と思っているらしいけど
+# read the doc は [index]なので設定
+master_doc = 'index'
